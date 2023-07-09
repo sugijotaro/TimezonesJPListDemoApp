9173d1967cee04bb5af5599df5826397dfb9ee33
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-07-09 14:44:46 +0900

    Initial Commit

---
+++ TimezonesJPListDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimezonesJPListDemoApp
+//
+//  Created by JotaroSugiyama on 2023/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimezonesJPListDemoApp/TimezonesJPListDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimezonesJPListDemoAppApp.swift
+//  TimezonesJPListDemoApp
+//
+//  Created by JotaroSugiyama on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct TimezonesJPListDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
